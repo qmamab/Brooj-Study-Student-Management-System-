a34c3682583c34cb2b62cd967c1adee78eafafce
Qamar Muneer Akbar <62990005+qmamab@users.noreply.github.com>
2025-10-18 21:34:42 +0400

    Add files via upload

---
+++ main_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MainAppConfig(AppConfig):
+    name = 'main_app'
+##COPYWRIGHT OF QAMAR MUNEER AKBAR & CS50 HARVARD 2022-CURRENT ALL WRIGHTS RESERVED##
---
+++ main_app/tests.py
@@ -0,0 +1,4 @@
+from django.test import TestCase
+
+# Create your tests here.
+##COPYWRIGHT OF QAMAR MUNEER AKBAR & CS50 HARVARD 2022-CURRENT ALL WRIGHTS RESERVED##
